353b04412c9372dd0c3a1e1bfe78f0edbe64513e
Leo Nelson <VleolionV@gmail.com>
2026-03-28 10:48:38 -0700

    Added the most goated front end the world has ever seen.

--- Excelnomics(Scripts)/app.py
+++ Excelnomics(Scripts)/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return "Hackathon Project"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9001)
+    app.run(host="0.0.0.0", port=9001, debug=True)
